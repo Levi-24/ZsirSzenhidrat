909d9cf22ae2f372eca54e91f099cc340470ed1a
Levente Major <leventemajor36@gmail.com>
2023-09-18 11:20:31 +0200

    Add project files.

---
+++ ZsirSzenhidrat/Kenyer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZsirSzenhidrat
+{
+    class Kenyer
+    {
+        public string Nev { get; set; }
+        public double Zsir { get; set; }
+        public double Zsenhidrat { get; set; }
+
+        public Kenyer (string BeolvasottSor)
+        {
+            var darabok = BeolvasottSor.Split(';');
+            this.Nev = darabok[0];
+            this.Zsir = double.Parse(darabok[1]);
+            this.Zsenhidrat = double.Parse(darabok[2]);
+        }
+    }
+}
---
+++ ZsirSzenhidrat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ZsirSzenhidrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kenyerek = new List<Kenyer>();
+            using var sr = new StreamReader(
+                path: "../../../src/Adat.txt",
+                Encoding.UTF8);
+
+            while (!sr.EndOfStream) kenyerek.Add(new Kenyer(sr.ReadLine()));
+
+            Console.WriteLine("1.Feladat");
+            foreach (var kenyer in kenyerek) Console.WriteLine($"{kenyer.Nev} {kenyer.Zsir} {kenyer.Zsenhidrat}");
+
+            Console.WriteLine("2.Feladat");
+            var minZsir = kenyerek.Min(k => k.Zsir);
+            foreach (var k in kenyerek) if (k.Zsir == minZsir) Console.WriteLine(k.Nev);
+        }
+    }
+}
